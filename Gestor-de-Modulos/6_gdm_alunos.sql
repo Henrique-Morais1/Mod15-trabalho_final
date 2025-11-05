04054757ce73ac179cda9f35b8830b5a8d9703bc
LightDark <goldenlightdark1st@gmail.com>
2025-11-05 16:59:00 +0000

    Alterações.

--- Gestor-de-Modulos/6_gdm_alunos.sql
+++ Gestor-de-Modulos/6_gdm_alunos.sql
@@ -6,7 +6,7 @@ CREATE TABLE alunos (
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     id_turma INT(11) UNSIGNED NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    n_aluno VARCHAR(20) UNSIGNED NOT NULL,
+    n_aluno INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_turma) REFERENCES turmas(id)
 );
