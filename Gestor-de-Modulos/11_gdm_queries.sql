18e6342dc447255070152dd99526cf505c1c269b
LightDark <goldenlightdark1st@gmail.com>
2025-11-24 19:02:51 +0000

    Criação da tabela Data e Queries.

---
+++ Gestor-de-Modulos/11_gdm_queries.sql
@@ -0,0 +1,17 @@
+USE gdm_system;
+
+SELECT * FROM turmas;
+
+SELECT * FROM cursos;
+
+SELECT * FROM disciplinas;
+
+SELECT * FROM modulos;
+
+SELECT * FROM alunos;
+
+SELECT * FROM modulos_capitalizados;
+
+SELECT * FROM historico_nome_cursos;
+
+SELECT * FROM curso_disciplina_modulos;
